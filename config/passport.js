1299b57b76892402959f35f5df7394e1c8bb30c9
DanielJ <dajaramillo@unicauca.edu.co>
2023-05-30 16:24:30 -0500

    Hasta el 30 de mayo

--- config/passport.js
+++ config/passport.js
@@ -23,9 +23,11 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser( (id, done)=>{
-   User.findById(id, (err, user)=>{
-        done(err, user);
+passport.deserializeUser(function(id, done) {
+    User.findById(id).then(function (user) {
+        done(null, user);
+    }).catch(function (err) {
+        done(err, null, { message: 'User does not exist' });
     });
 });
 
--- routes/datos.js
+++ routes/datos.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 
-router.get('/usuarios', (req, res)=> {
+router.get('/usuarios', async (req, res)=> {
   //res.send('Estos son los datos');
-  res.render('users/dato');
+    const datos = await User.find();
+    res.render('users/dato', { datos });
 });
 
 
